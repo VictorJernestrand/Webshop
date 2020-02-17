5bd3216cb6344b5e172bc211eb9fc3e24b305a26
uma Prithi <umaprithi@outlook.com>
2020-02-17 10:32:52 +0100

    1.Created Product Controller  and add the index mtd    2.Created  view for the index method

---
+++ Webshop/Webshop/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webshop.Context;
+
+namespace Webshop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db=new WebshopContext())
+            {
+                var query = (from product in db.Products
+                             select product).ToList();
+                return View(query);
+            }
+                
+        }
+    }
+}
