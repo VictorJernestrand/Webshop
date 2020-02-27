a405dd718f93cff0a487ecfa53ef563053c58bcd
Nick Swardh <nick@nswardh.com>
2020-02-27 16:12:43 +0100

    Configured startup.cs for supporting WebAPI requests

--- Webshop/Webshop/ViewComponents/CategoryMenuListViewComponent.cs
+++ Webshop/Webshop/ViewComponents/CategoryMenuListViewComponent.cs
@@ -3,19 +3,23 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Webshop.Context;
 using Webshop.Models;
+using Webshop.Services;
 
 namespace Webshop.ViewComponents
 {
     public class CategoryMenuListViewComponent : ViewComponent
     {
         private readonly WebshopContext _context;
+        private readonly IHttpClientFactory _clientFactory;
 
-        public CategoryMenuListViewComponent(WebshopContext context)
+        public CategoryMenuListViewComponent(WebshopContext context, IHttpClientFactory clientFactory)
         {
             _context = context;
+            _clientFactory = clientFactory;
         }
 
         public async Task<IViewComponentResult> InvokeAsync(
@@ -25,9 +29,17 @@ namespace Webshop.ViewComponents
             return View(items);
         }
 
-
+        
         private async Task<IEnumerable<Category>> GetAllCategoriesAsync()
             => await _context.Categories.ToListAsync();
 
+        /*
+        // Get all categories from the WebAPI
+        private async Task<IEnumerable<Category>> GetAllCategoriesFromWebAPIAsync()
+        {
+            WebAPIHandler<Category> webAPI = new WebAPIHandler<Category>(_clientFactory, "https://localhost:44305/api/categories");
+            return await webAPI.GetAllFromWebAPIAsync();
+        }
+        */
     }
 }
