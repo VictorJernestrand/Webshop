e74b92a4c3ac1fd28a832ed72adff153bfc571d0
Nick Swardh <nick@nswardh.com>
2020-03-02 19:28:41 +0100

    Added functionality for adding,updating and removing Brands

---
+++ Webshop/Webshop/Models/EditBrandModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    public class EditBrandModel
+    {
+        public IEnumerable<Brand> BrandsCollection { get; set; }
+        
+        public Brand Brand { get; set; }
+    }
+}
