8a397d877b17c52a490c2b4156c890b1682f224c
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-03-03 11:16:03 +0100

    edit contact form messages

--- Webshop/Webshop/Controllers/ContactController.cs
+++ Webshop/Webshop/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Webshop.Models;
 
@@ -13,6 +14,33 @@ namespace Webshop.Controllers
         public IActionResult Index()
         {
             return View(ContactModel);
+        }
+
+        [HttpPost, ActionName("Index")]
+        [ValidateAntiForgeryToken]
+        public IActionResult PostMessage([Bind]ContactModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    ContactModel newMessage = new ContactModel()
+                    {
+                        Email = model.Email,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        PhoneNumber = model.PhoneNumber,
+                        Message = model.Message
+                    };
+                    TempData["MessageSuccess"] = "Tack för ditt meddelande!";
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(model);
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
