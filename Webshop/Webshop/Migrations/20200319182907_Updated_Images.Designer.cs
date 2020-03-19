0051ca0dd7c0bb2a805f1072a55fc714f8691bb3
uma Prithi <umaprithi@outlook.com>
2020-03-19 19:30:13 +0100

    Updated Images in WebshopContext

--- Webshop/Webshop/Migrations/20200319182907_Updated_Images.Designer.cs
+++ Webshop/Webshop/Migrations/20200319182907_Updated_Images.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webshop.Context;
 
 namespace Webshop.Migrations
 {
     [DbContext(typeof(WebshopContext))]
-    partial class WebshopContextModelSnapshot : ModelSnapshot
+    [Migration("20200319182907_Updated_Images")]
+    partial class Updated_Images
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
